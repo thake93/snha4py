ba5fbe65d8c24e3f1dfef8ed7a4d6297d9d222fd
thake93 <thake@uni-potsdam.de>
2023-01-25 13:27:01 +0100

    setup for pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 import setuptools
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setuptools.setup(
     name="snha4py",  # Replace with your own username
-    version="0.0.4",
+    version="0.0.1",
     author="Tim Hake",
-    author_email="thake@uni-potsdam.de",
+    author_email="timhake@icloud.com",
     description="St. Nicholas House algorithm for Python",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
     url="https://github.com/thake93/snha4py",
+    project_urls={
+        "Documentation": "https://htmlpreview.github.io/?https://github.com/thake93/snha4py/blob/main/docs/__init__.html"
+    },
     packages=["snha4py"],
-    license="LICENSE",
+    license="MIT License",
+    license_files=["LICENSE"],
     # classifiers=[
     #    "Programming Language :: Python :: 3",
     #    "License :: OSI Approved :: MIT License",
