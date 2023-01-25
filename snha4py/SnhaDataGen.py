1070940d1a14234cd694e24fc894737ba4f47b14
thake93 <thake@uni-potsdam.de>
2023-01-25 18:07:21 +0100

    fixed link

--- snha4py/SnhaDataGen.py
+++ snha4py/SnhaDataGen.py
@@ -3,7 +3,7 @@
 """
 St. Nicolas House Algorithm data generation.
 
-The module generates data correlated data along a directed graph based on the Monte Carlo approach. For further inforation see [Novine et al. (2022) (https://doi.org/10.52905/hbph2021.3.26).
+The module generates data correlated data along a directed graph based on the Monte Carlo approach. For further inforation see [Novine et al. (2022)](https://doi.org/10.52905/hbph2021.3.26).
 """
 import numpy as np
 import pandas as pd
