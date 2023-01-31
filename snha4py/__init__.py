d5cfb9b174987facf65342d10a53f13d2c2f79e2
thake93 <thake@uni-potsdam.de>
2023-01-31 12:20:40 +0100

    fix md syntax

--- snha4py/__init__.py
+++ snha4py/__init__.py
@@ -1,12 +1,13 @@
 #! /usr/bin/env python3
 
 """
-API of snha4py
+# API of snha4py
 
 Documentation of the St. Nicolas House algorithm for python3. 
 [Back to github](https://github.com/thake93/snha4py/)
 
-Classes
+## Classes 
+
 *Note*:
 
 - in green: public methods
